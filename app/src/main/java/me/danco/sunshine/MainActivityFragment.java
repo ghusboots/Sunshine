8b357e55e608d3b28853fe048856d57f7905e2a0
Dan Coulter <ghusboots@gmail.com>
2015-06-04 01:04:54 -0700

    List with fake static data

--- app/src/main/java/me/danco/sunshine/MainActivityFragment.java
+++ app/src/main/java/me/danco/sunshine/MainActivityFragment.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 /**
@@ -16,8 +21,31 @@ public class MainActivityFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        String[] forecastArray = {
+                "Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+        };
+
+        ArrayList<String> forecasts = new ArrayList<>(Arrays.asList(forecastArray));
+        ArrayAdapter<String> forecastAdapter = new ArrayAdapter<>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, forecasts);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView forecastList = (ListView)rootView.findViewById(R.id.listview_forecast);
+        forecastList.setAdapter(forecastAdapter);
+
+        return rootView;
     }
 }
